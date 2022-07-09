fb9c7397ade1d76957e2780d5597d664e6dc9888
Nadha-Jabeen-T <nadha.jabeen@gmail.com>
2022-07-09 14:00:38 +0530

    Initial content and layout finished

--- lib/main.dart
+++ lib/main.dart
@@ -9,22 +9,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: ListView(
-          padding: const EdgeInsets.all(8),
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Colors.brown.shade800,
-              child: const Text('Nadha'),
-              backgroundImage: NetworkImage(
-                  'https://api.time.com/wp-content/uploads/2018/12/zendaya-time100-2022.jpg'),
-            ),
-            Column(),
-            Row(),
-            ElevatedButton(
-              onPressed: null,
-              child: Text('Follow'),
-            ),
-          ],
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              const CircleAvatar(
+                //backgroundColor: Colors.brown.shade800,
+                //child: const Text('Nadha'),
+                minRadius: 90,
+                backgroundImage: NetworkImage(
+                    'https://api.time.com/wp-content/uploads/2018/12/zendaya-time100-2022.jpg'),
+              ),
+              Column(
+                children: <Widget>[
+                  Text('Nadha Jabeen'),
+                  Text('Flutter developer'),
+                  Text(
+                      'Associate Flutter developer @Google\nFounder of ScrapOut')
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Text('2390'),
+                        Text('Followers'),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Text('1000'),
+                        Text('Following'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              ElevatedButton(
+                onPressed: null,
+                child: Text('Follow'),
+              ),
+            ],
+          ),
         ),
       ),
     );
